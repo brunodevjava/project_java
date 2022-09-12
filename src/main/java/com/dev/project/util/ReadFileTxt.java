fb361c1babdfa9258463069e5bb373f0dfb3f817
Bruno Dev <brunodeveloper2022@gmail.com>
2022-09-12 11:03:32 -0300

    add READ_FILE RP - SUBSTRING

--- src/main/java/com/dev/project/util/ReadFileTxt.java
+++ src/main/java/com/dev/project/util/ReadFileTxt.java
@@ -10,19 +10,34 @@ public class ReadFileTxt {
 
     public String ReadFile(MultipartFile file) throws IOException {
 
-        File convFile = new File( file.getOriginalFilename() );
-        FileOutputStream fos = new FileOutputStream( convFile );
-        fos.write( file.getBytes() );
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
         fos.close();
         //String content = new String(file.getBytes());
 
         try (FileReader fr = new FileReader(convFile);
              BufferedReader br = new BufferedReader(fr);) {
             String line;
+            int index = 1;
             while ((line = br.readLine()) != null) {
-                if(line.contains("1D")){
-                    System.out.println(line.codePoints());
+//System.out.println("00"+(index++)+"D");
+
+                String in = "00"+index+"D";
+                if(!line.contains(in)){
+//                    System.out.println("Linha pulada");
+
                 }
+                if (line.contains(in)) {
+                    index++;
+                    System.out.println("LINHA PARA MODIFICACAO: "+line);
+                    System.out.println("SALÁRIO CONTRIBUIÇÃO "+line.substring(106, 120));
+                    System.out.println("BASE DE CÁLCULO F.G.T.S. "+line.substring(211, 225));
+                    System.out.println(line.replaceAll(line.substring(106, 120), line.substring(211, 225)));
+                    System.out.println("########################");
+
+                }
+
 
             }
         } catch (IOException e) {
